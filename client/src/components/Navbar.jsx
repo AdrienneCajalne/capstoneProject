f5937e2837feb63c65ddd26d1746f3c9815dac4f
Lorenz Cajalne <lorenzcajalneofficial@gmail.com>
2023-09-15 21:58:07 +0800

    qwe

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,12 @@
 import { Link } from "react-router-dom"
 
+export { default as Article } from './article/Article';
+export { default as Brand } from './brand/Brand';
+export { default as CTA } from './cta/CTA';
+export { default as Feature } from './feature/Feature';
+export { default as Navbar } from './navbar/Navbar';
+
+
 export default function Navbar() {
     return (
         <nav>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
 
-export default function Home() {
-    return (
-        <div></div>
-    )
-}
+
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from '../containers';
+import { CTA, Brand, Navbar } from '../components/Navbar';
+
+import './Home.css';
+
+const App = () => (
+<div className="App">
+    <div className="gradient__bg">
+        <Navbar />
+        <Header />
+    </div>
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
+    </div>
+);
+
+export default App;
+
 
