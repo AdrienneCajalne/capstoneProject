a27c501c631c19930a1d5ea5eb9b7eac7f637710
Lorenz Cajalne <lorenzcajalneofficial@gmail.com>
2023-09-17 22:43:18 +0800

    login done

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -3,6 +3,10 @@ import axios from 'axios'
 import {toast} from 'react-hot-toast'
 import { useNavigate } from 'react-router-dom'
 import './Login.css'
+import { FaEyeSlash,FaFacebookF, FaEye } from "react-icons/fa"
+import googleLogo from '../assets/googleLogo.png'
+import { Link } from 'react-router-dom'
+
 
 export default function Login() {
     const navigate = useNavigate()
@@ -26,19 +30,92 @@ export default function Login() {
                     navigate('/dashboard')
                 }
             } catch (error) {
-                
+                console.log(error)
             }
     }
 
+    const forms = document.querySelector(".forms");
+    const pwShowHide = document.querySelectorAll(".eye-icon");
+
+    pwShowHide.forEach(eyeIcon => {
+        let isPasswordVisible = false;
+        const pwFields = Array.from(eyeIcon.parentElement.parentElement.querySelectorAll(".password"));
+
+        eyeIcon.addEventListener("click", () => {
+            isPasswordVisible = !isPasswordVisible;
+            
+            pwFields.forEach(password => {
+                password.type = isPasswordVisible ? "text" : "password";
+            });
+
+            if (isPasswordVisible) {
+                eyeIcon.classList.replace("FaEyeSlash", "FaEye");
+            } else {
+                eyeIcon.classList.replace("FaEye", "FaEyeSlash");
+            }
+        });
+    });
+
+
     return (
-        <div>
-            <form onSubmit={loginUser}>
-                <label>Email</label>
-                    <input type='email' placeholder='Enter email...' value={data.email} onChange={(e) => setData({...data, email: e.target.value})} />
-                <label>Password</label>
-                    <input type='password' placeholder='Enter password...' value={data.password} onChange={(e) => setData({...data, password: e.target.value})} />
-                <button type='submit'>Login</button>
-            </form>
-        </div>
+
+        <body>
+            
+            <section className="container forms">
+                <div className="form login">
+                    <div className="form-content">
+                        <header>Login</header>
+
+                        <form onSubmit={loginUser}>   
+                        {/* loginEmail */}
+                            <div className="field input-field">
+                                <input type="email" placeholder="Email"  className="input" value={data.email} onChange={(e) => setData({...data, email: e.target.value})} />
+                            </div>
+                        {/* loginPassword */}
+                            <div className="field input-field">
+                                <input type="password" placeholder="Password"  className="password"value={data.password} onChange={(e) => setData({...data, password: e.target.value})} />
+                                <FaEyeSlash className="eye-icon"/>
+                                
+                            </div>
+
+                            <div className="form-link">
+                                <a href="#" className="forgot-pass">Forgot password?</a>
+                            </div>
+                        {/* loginButton */}
+                            <div className="field button-field">
+                                <button type="submit">Login</button>
+                            </div>
+                        </form>
+
+                        <div className="form-link">
+                                <span>Dont have an account? <Link to="/register">Register</Link></span>
+                            </div>
+                    </div>
+
+                    <div className="line"></div>
+
+                    <div className="media-options">
+                        <a href="#" className="field facebook">
+                        <FaFacebookF className="facebook-icon"/>
+                        <span>Login with Facebook</span>
+                        </a>
+                    </div>
+
+                    <div className="media-options">
+                        <a href="#" className="field google">
+                        <img src={googleLogo} alt="Logo" className="google-img"/>
+                        <span>Login with Google</span>
+                        </a>
+                    </div>
+
+                </div>
+            </section>
+
+
+
+
+
+
+        </body>
     )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -3,6 +3,10 @@ import axios from 'axios'
 import { toast } from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom'
 import './Register.css'
+import 'boxicons'
+import { FaEyeSlash, FaEye, FaFacebookF, } from "react-icons/fa";
+import googleLogo from '../assets/googleLogo.png'
+import { Link } from 'react-router-dom';
 
 export default function Register() {
     const navigate = useNavigate()
@@ -31,17 +35,87 @@ export default function Register() {
         }
     }
 
+    const forms = document.querySelector(".forms");
+    const pwShowHide = document.querySelectorAll(".eye-icon");
+    
+    pwShowHide.forEach(eyeIcon => {
+        let isPasswordVisible = false;
+        const pwFields = Array.from(eyeIcon.parentElement.parentElement.querySelectorAll(".password"));
+    
+        eyeIcon.addEventListener("click", () => {
+            isPasswordVisible = !isPasswordVisible;
+            
+            pwFields.forEach(password => {
+                password.type = isPasswordVisible ? "text" : "password";
+            });
+    
+            if (isPasswordVisible) {
+                eyeIcon.classList.replace("FaEyeSlash", "FaEye");
+            } else {
+                eyeIcon.classList.replace("FaEye", "FaEyeSlash");
+            }
+        });
+    });
+
     return (
-        <div>
-            <form className="form"  onSubmit={registerUser}>
-                <label>Name</label>
-                <input type='text' placeholder='Enter name...' value={data.name} onChange={(e) => setData({...data, name: e.target.value})} />
-                <label>Email</label>
-                <input type='email' placeholder='Enter email...' value={data.email} onChange={(e) => setData({...data, email: e.target.value})} />
-                <label>Password</label>
-                <input type='password' placeholder='Enter password...' value={data.password} onChange={(e) => setData({...data, password: e.target.value})} />
-                <button type='submit'>Submit</button>
-            </form>
-        </div>
+        <body>
+            
+            <section className="container forms">
+                <div className="form signup">
+                    <div className="form-content">
+                        <header>Sign-Up</header>
+
+                        <form onSubmit={registerUser}>
+                        {/* registerUsername */}
+                        <div className="field input-field">
+                        <input type='text' placeholder='Username' value={data.name} onChange={(e) => setData({...data, name: e.target.value})} />
+                            </div>
+                        {/* registerEmail */}
+                            <div className="field input-field">
+                            <input type='email' placeholder='Email' value={data.email} onChange={(e) => setData({...data, email: e.target.value})} />
+                            </div>
+                        {/* registerPassword */}
+                            <div className="field input-field">
+                            <input type='password' placeholder='Password' className="password" value={data.password} onChange={(e) => setData({...data, password: e.target.value})} />
+                                <FaEyeSlash className="eye-icon"/>
+                                
+                            </div>
+
+                        {/* registerButton */}
+                            <div className="field button-field">
+                                <button type="submit">Continue</button>
+                            </div>
+                        </form>
+
+                        <div className="form-link">
+                                <span>Already have an account? <Link to="/login">Login</Link></span>
+                            </div>
+                    </div>
+
+                    <div className="line"></div>
+
+                    <div className="media-options">
+                        <a href="#" className="field facebook">
+                        <FaFacebookF className="facebook-icon"/>
+                        <span>Login with Facebook</span>
+                        </a>
+                    </div>
+
+                    <div className="media-options">
+                        <a href="#" className="field google">
+                        <img src={googleLogo} alt="Logo" className="google-img"/>
+                        <span>Login with Google</span>
+                        </a>
+                    </div>
+
+                </div>
+            </section>
+
+
+
+
+
+
+        </body>
     )
 }
